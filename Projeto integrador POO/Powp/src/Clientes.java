a8e219695b4b5b1e1bf0c1ea411d6aa7b7fd9b6c
João Luccas Marques <107486816+JoaoLu2cas@users.noreply.github.com>
2024-09-12 20:57:52 -0300

    POO PI
    Projeto integrador de Programação orientada a objetos

---
+++ Projeto integrador POO/Powp/src/Clientes.java
@@ -0,0 +1,25 @@
+public class Clientes extends PessoaJuridica {
+
+	private Clientes codCliente;
+
+	private double limiteCredito;
+
+	private boolean ativo;
+
+	public double getLimite() {
+		return 0;
+	}
+
+	public boolean getAtivo() {
+		return false;
+	}
+
+	public String cadastrarCliente() {
+		return null;
+	}
+
+	public String editarDados() {
+		return null;
+	}
+
+}
